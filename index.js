1c18f0222ce1d6fcf09bfdcd910a52e95a493259
Rachel <rstark.smith@gmail.com>
2022-03-20 09:04:58 -0500

    commit tree c6488725040539a9c9555690a6dd6a21298143fc

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+//const upperCaseCustomerName => customerName.toUpperCase();
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "customer";
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "badCustomer";
+}
